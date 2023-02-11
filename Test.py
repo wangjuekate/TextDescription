54f43958d0432c8caf3740bb07365dbcd5b8fe02
Kate Jue Wang <wangjuekate@gmail.com>
2023-02-11 08:00:57 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -10,6 +10,23 @@ import time
 import concurrent.futures
 from pymongo import MongoClient
 
+def caldistance(corpusa, corpusb,dicta, dictb ):
+    listworda =pd.DataFrame()
+    for id, freq in corpusa:
+        itemdf = pd.DataFrame([dicta[id],freq])
+        listworda = pd.concat([listworda, itemdf.transpose()],axis =0)
+    listworda.columns =['word','freq']
+
+    listwordb =pd.DataFrame()
+    for id, freq in corpusb:
+        itemdf = pd.DataFrame([dictb[id],freq])
+        listwordb = pd.concat([listwordb, itemdf.transpose()],axis =0)
+    listwordb.columns=['word','freq']
+
+    total_merge = listworda.merge(listwordb, on='word', how='outer', indicator=True)
+    total_merge= total_merge.fillna(0)
+    distance = sum((total_merge['freq_x']- total_merge['freq_y'])**2)
+    return(distance)
 
 panelafterincumbent = pd.read_csv("~/ExemplarincumbentpanelDID_0130.csv",
 sep=",")
@@ -25,17 +42,45 @@ dbname = client['gplayall']
 
 allappdescrip = pd.DataFrame()
 exemplardata = pd.DataFrame()
-for i in range(1,13,1):
-    print(i)
-    namedata = 'moredescribe'+str(i)
-    collection = dbname[namedata]
-    item_details = collection.find()
-    allmonthlydata = pd.DataFrame(item_details)
-    #get appdescriptions
-    month= allmonthlydata[allmonthlydata['appId'].isin(applist)]
-    month = month[['appId','corpus','dictionary','month']]
-    allappdescrip = pd.concat([allappdescrip,month],axis =0)
+i=5
+namedata = 'moredescribe'+str(i)
+collection = dbname[namedata]
+item_details = collection.find()
+allmonthlydata = pd.DataFrame(item_details)
+#get appdescriptions
+monthdata= allmonthlydata[allmonthlydata['appId'].isin(applist)]
+monthdata = monthdata[['appId','corpus','dictionary',i]]
+allappdescrip = pd.concat([allappdescrip,monthdata],axis =0)
 
-    month= allmonthlydata[allmonthlydata['appId'].isin(exemplarid)]
-    month = month[['appId','corpus','dictionary','month']]
-    exemplardata = pd.concat([exemplardata,month],axis =0)
+monthdata= allmonthlydata[allmonthlydata['appId'].isin(exemplarid)]
+monthdata = monthdata[['appId','corpus','dictionary',i]]
+exemplardata = pd.concat([exemplardata,monthdata],axis =0)
+
+print(exemplardata['month'].value_counts())
+
+
+test = panelafterincumbent[panelafterincumbent['month']==i]
+
+
+for index, row in test.iterrows():
+    appid = [row['appID']]
+    exemplarid =  [row['exemplarID']]
+    month = row['month']
+    try:
+        aside= allappdescrip[(allappdescrip['appId'].isin(appid) &  allappdescrip['month']==month)]
+        corpusa = aside.iloc[0,1]
+        dicta = aside.iloc[0,2]
+        bside= allappdescrip[(allappdescrip['appId'].isin(exemplarid) &  allappdescrip['month']==month)]
+        corpusb = bside.iloc[0,1]
+        dictb = bside.iloc[0,2]
+        distance = caldistance(corpusa, corpusb,dicta, dictb )
+        test.loc[index, 'distance' ] =  distance
+    except: 
+        test.loc[index, 'distance' ] =  0
+
+
+namefile = "test"+i 
+
+test.to_csv('~/namefile.csv', 
+sep=',',index=False)
+
