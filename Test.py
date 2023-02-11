1c49f663b07a639076e84d2fa0701b1742f20d40
Kate Jue Wang <wangjuekate@gmail.com>
2023-02-11 09:45:09 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -28,20 +28,10 @@ def caldistance(corpusa, corpusb,dicta, dictb ):
     distance = sum((total_merge['freq_x']- total_merge['freq_y'])**2)
     return(distance)
 
-panelafterincumbent = pd.read_csv("~/ExemplarincumbentpanelDID_0130.csv",
-sep=",")
-
-applist = panelafterincumbent['appID'].drop_duplicates()
-exemplarid = panelafterincumbent['exemplarID'].drop_duplicates()
-
-
-#get the description summarize: 
+#access database
 
 client = MongoClient()
 dbname = client['gplayall']
-
-allappdescrip = pd.DataFrame()
-exemplardata = pd.DataFrame()
 i=2
 namedata = 'moredescribe'+str(i)
 collection = dbname[namedata]
@@ -49,41 +39,48 @@ item_details = collection.find()
 allmonthlydata = pd.DataFrame(item_details)
 print(allmonthlydata.iloc[1])
 print(allmonthlydata['month'].value_counts())
-#get appdescriptions
+
+#get app list
+
+panelafterincumbent = pd.read_csv("~/ExemplarincumbentpanelDID_0130.csv",
+sep=",")
+
+applist1 = panelafterincumbent['appID'].drop_duplicates()
+exemplarid = panelafterincumbent['exemplarID'].drop_duplicates()
+applist= applist1.values.tolist() + exemplarid.values.tolist()
+
+#get the description summarize: 
+allappdescrip = pd.DataFrame()
 monthdata= allmonthlydata[allmonthlydata['appId'].isin(applist)]
 monthdata = monthdata[['appId','corpus','dictionary','month']]
 allappdescrip = pd.concat([allappdescrip,monthdata],axis =0)
 
-monthdata= allmonthlydata[allmonthlydata['appId'].isin(exemplarid)]
-monthdata = monthdata[['appId','corpus','dictionary','month']]
-exemplardata = pd.concat([exemplardata,monthdata],axis =0)
+print(allappdescrip['month'].value_counts())
 
-print(exemplardata['month'].value_counts())
-
+#calculate
 
 test = panelafterincumbent[panelafterincumbent['month']==i]
 print(test.iloc[1])
 
+
 for index, row in test.iterrows():
     appid = [row['appID']]
+    print(appid)
     exemplarid =  [row['exemplarID']]
+    print(exemplarid)
     month = row['month']
-    try:
-        aside= allappdescrip[(allappdescrip['appId'].isin(appid) &  allappdescrip['month']==month)]
-        print(aside)
-        corpusa = aside.iloc[0,1]
-        dicta = aside.iloc[0,2]
-        bside= exemplardata[(exemplardata['appId'].isin(exemplarid) &  exemplardata['month']==month)]
-        print(bside)
-        corpusb = bside.iloc[0,1]
-        dictb = bside.iloc[0,2]
-        distance = caldistance(corpusa, corpusb,dicta, dictb )
-        print(distance)
-        test.loc[index, 'distance' ] =  distance
-    except: 
-        test.loc[index, 'distance' ] =  0
 
-print(test['distance'])
+    aside= allappdescrip[(allappdescrip['appId'].isin(appid)) &  (allappdescrip['month']==month)]
+    print(aside)
+    corpusa = aside.iloc[0,1]
+    dicta = aside.iloc[0,2]
+    bside= allappdescrip[(allappdescrip['appId'].isin(exemplarid)) &  (allappdescrip['month']==month)]
+    print(bside)
+    corpusb = bside.iloc[0,1]
+    dictb = bside.iloc[0,2]
+    distance = caldistance(corpusa, corpusb,dicta, dictb )
+    test.loc[index, 'distance' ] =  distance
+    print(distance)
 
 namefile = "test"+str(i)
 
