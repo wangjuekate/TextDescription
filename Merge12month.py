e55481e0d562e0b1bb3523f9aedf40a9c1ac7b9d
Kate Jue Wang <wangjuekate@gmail.com>
2023-02-11 19:28:32 -0500

    Update and rename Test1.py to Merge12month.py

---
+++ Merge12month.py
@@ -0,0 +1,21 @@
+
+import pymongo
+import pandas as pd
+from json import loads
+import numpy as np
+import math
+import json
+import time
+from pymongo import MongoClient
+
+import tqdm                                                                                                   
+import numpy as np
+import pandas as pd
+
+panel = pd.DataFrame()
+
+for i in range(1,13,1):
+    test = pd.read_csv("~/test", sep=",")
+    
+
+
