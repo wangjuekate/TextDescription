8b983927870df3babebedaa6d674fc930ce74d2e
Kate Jue Wang <wangjuekate@gmail.com>
2023-02-14 12:13:33 -0500

    Update create_input_files.py

--- IdentifySub_Com/create_input_files.py
+++ IdentifySub_Com/create_input_files.py
@@ -2,11 +2,11 @@
 from utils1 import create_input_files, train_word2vec_model
 
 if __name__ == '__main__':
-    create_input_files(csv_folder='/Users/jjw6286/Downloads/Machinelearning/a-PyTorch-Tutorial-to-Text-Classification/datasettotrain',
-                       output_folder='/Users/jjw6286/Downloads/Machinelearning/a-PyTorch-Tutorial-to-Text-Classification',
+    create_input_files(csv_folder='~/TextDescription/IdentifySub_Com/datatraining',
+                       output_folder='~/TextDescription/IdentifySub_Com/',
                        sentence_limit=15,
                        word_limit=20,
                        min_word_count=5)
 
-    train_word2vec_model(data_folder='/Users/jjw6286/Downloads/Machinelearning/a-PyTorch-Tutorial-to-Text-Classification',
+    train_word2vec_model(data_folder='~/TextDescription/IdentifySub_Com/',
                          algorithm='skipgram')
